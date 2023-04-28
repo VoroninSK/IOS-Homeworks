6ec56d3d0c24361f579b0580fa286e0421df9930
Сергей Воронин <sergei@MacBook-Pro-Sergej.local>
2023-04-28 14:36:39 +0800

    create project

---
+++ IOSHomeworks/IOSHomeworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSHomeworksApp.swift
+//  IOSHomeworks
+//
+//  Created by Сергей Воронин on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSHomeworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
